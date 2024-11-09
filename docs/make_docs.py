92fc1c7bd9f9ea42ddbcfa5f531e052d892419f6
simo_lenovo <gramegna.simone@gmail.com>
2024-11-09 12:50:31 +0100

    added docs

--- docs/make_docs.py
+++
@@ -1,6 +0,0 @@
-from pylode.profiles.ontpub import OntPub
-
-od = OntPub(ontology="../peo_ontology_ttl.ttl")
-od.make_html(destination="peo_ontology_doc.html")
-
-print("\nOntology documentation created successfully!")
