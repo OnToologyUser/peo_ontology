2d4475789ea0c350a1e0b5d2c81f4242a9effbe5
simo_lenovo <gramegna.simone@gmail.com>
2024-11-18 17:33:31 +0100

    minor changes

--- scripts/make_queries.py
+++ scripts/make_queries.py
@@ -8,22 +8,9 @@ g.parse(rdf_file, format="xml")
 
 # Query SPARQL
 sparql_query = """
-SELECT ?llm  ?llmType ?company 
+SELECT DISTINCT ?property ?value
 WHERE {
-    # Retrieve companies
-    ?company rdf:type <https://w3id.org/peo#company> .
-    
-    # Link companies to LLM instances through the develops relation
-    ?company <https://w3id.org/peo#develops> ?llm .
-    
-    # Ensure the LLM is an instance of a subclass of LargeLanguageModel
-    ?llm rdf:type ?llmType .
-    ?llmType rdfs:subClassOf <https://w3id.org/peo#large_language_model> .
-    
-    # Optionally retrieve labels for companies, LLMs, and LLM types
-    OPTIONAL { ?company rdfs:label ?companyLabel . }
-    OPTIONAL { ?llm rdfs:label ?llmLabel . }
-    OPTIONAL { ?llmType rdfs:label ?llmTypeLabel . }
+    <https://w3id.org/peo#large_language_model> ?property ?value .
 }
 """
 
